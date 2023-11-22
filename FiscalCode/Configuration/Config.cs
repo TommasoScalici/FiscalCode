3bf269c2099dd393ace5f926a5fa9712066df444
Tommaso Scalici <tommaso.scalici.1991@gmail.com>
2023-11-22 17:30:04 +0100

    Implemented card actions (copy, edit, delete) #2

--- FiscalCode/Configuration/Config.cs
+++ FiscalCode/Configuration/Config.cs
@@ -1,5 +1,7 @@
 ﻿using System.Text.Json;
 
+using MudBlazor;
+
 namespace FiscalCode.Configuration;
 public static class Config
 {
@@ -8,4 +10,12 @@ public static class Config
         using var stream = await FileSystem.OpenAppPackageFileAsync("secrets.json");
         return stream != null ? (await JsonSerializer.DeserializeAsync<Secret>(stream))?.ServiceApiKey : null;
     }
+
+    public static DialogOptions DialogOptions { get; } = new()
+    {
+        CloseButton = true,
+        CloseOnEscapeKey = true,
+        MaxWidth = MaxWidth.Small,
+        Position = DialogPosition.TopCenter
+    };
 }
