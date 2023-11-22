79426e95b6ab5c3cc28d50e70abb33a111589f48
Tommaso Scalici <tommaso.scalici.1991@gmail.com>
2023-11-22 20:58:44 +0100

    Implemented edit feature and load/save when fiscal codes are updated #1 #4

--- FiscalCode/Services/FiscalCodeDataService.cs
+++ FiscalCode/Services/FiscalCodeDataService.cs
@@ -1,18 +1,63 @@
-﻿using System.Text.Json;
+﻿using System.Diagnostics;
+using System.Text.Json;
 
 using FiscalCode.Configuration;
 using FiscalCode.Data;
 
 namespace FiscalCode.Services;
-public class FiscalCodeDataService(HttpClient http)
+public class FiscalCodeDataService
 {
     private static readonly JsonSerializerOptions options = new() { PropertyNameCaseInsensitive = true };
-    private readonly HttpClient http = http;
+    private static readonly string dataJsonFileName = "data.json";
+    private static readonly string fullPath = $"{FileSystem.AppDataDirectory}{Path.DirectorySeparatorChar}{dataJsonFileName}";
+    private static string? token;
+    private readonly HttpClient http;
 
+
+    public FiscalCodeDataService(HttpClient http)
+    {
+        this.http = http;
+        this.http.BaseAddress = new("http://api.miocodicefiscale.com");
+    }
+
+
+    public static async Task<IEnumerable<FiscalCodeDTO>> LoadFiscalCodesFromFileAsync()
+    {
+        try
+        {
+            var json = await File.ReadAllBytesAsync(fullPath);
+            using var stream = new MemoryStream(json);
+            var list = await JsonSerializer.DeserializeAsync<IEnumerable<FiscalCodeDTO>>(stream);
+            return list ?? Enumerable.Empty<FiscalCodeDTO>();
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine(ex);
+        }
+
+        return Enumerable.Empty<FiscalCodeDTO>();
+    }
+
+    public static async Task SaveFiscalCodesToFileAsync(IEnumerable<FiscalCodeDTO> list)
+    {
+        try
+        {
+            var json = JsonSerializer.Serialize(list);
+            await File.WriteAllTextAsync(fullPath, json);
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine(ex);
+        }
+    }
 
     public async Task<string> CalculateFiscalCodeAsync(FiscalCodeDTO dto)
     {
-        http.BaseAddress = new("http://api.miocodicefiscale.com");
+        if (string.IsNullOrEmpty(token))
+            token = await Config.GetServiceApiTokenAsync();
+
+        if (token == null)
+            throw new NullReferenceException(nameof(token));
 
         if (dto.BirthDate == null)
             throw new NullReferenceException(nameof(dto.BirthDate));
@@ -21,7 +66,6 @@ public class FiscalCodeDataService(HttpClient http)
             throw new NullReferenceException(nameof(dto.BirthPlace));
 
         var birthdate = dto.BirthDate.Value;
-        var token = await Config.GetServiceApiTokenAsync();
 
         var queryString = $"calculate?lname={dto.LastName}&fname={dto.FirstName}&gender={dto.Sex}"
                             + $"&city={dto.BirthPlace.Name}&state={dto.BirthPlace.State}"
@@ -29,7 +73,7 @@ public class FiscalCodeDataService(HttpClient http)
                             + $"&access_token={token}";
 
         var response = await http.GetAsync(queryString);
-        var stream = await response.Content.ReadAsStreamAsync();
+        using var stream = await response.Content.ReadAsStreamAsync();
 
         var apiResponse = await JsonSerializer.DeserializeAsync<FiscalCodeAPIResponse>(stream, options);
 
