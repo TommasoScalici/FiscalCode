b88df33352642294b0846142d067b5acdb0775c6
Tommaso Scalici <tommaso.scalici.1991@gmail.com>
2023-11-24 20:56:02 +0100

    Code refactoring. Added Progress/Spinner indicator when the app is busy

--- FiscalCode/Features/PhotoScannerOCR.cs
+++ FiscalCode/Features/PhotoScannerOCR.cs
@@ -3,29 +3,48 @@
 using FiscalCode.Data;
 using FiscalCode.Services;
 
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision.Models;
+
 namespace FiscalCode.Features;
 public class PhotoScannerOCR(FiscalCodeOCRAnalyzerService fiscalCodeOCRAnalyzerService)
 {
     private readonly FiscalCodeOCRAnalyzerService fiscalCodeOCRAnalyzerService = fiscalCodeOCRAnalyzerService;
 
 
-    public async Task<FiscalCodeDTO> GetFiscalCodeDTOFromOCRScanAsync()
+    public static async Task<FileResult?> CapturePhotoAsync()
     {
         try
         {
             if (MediaPicker.Default.IsCaptureSupported)
-            {
-                var photo = await MediaPicker.Default.CapturePhotoAsync();
+                return await MediaPicker.Default.CapturePhotoAsync();
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine(ex);
+        }
 
-                if (photo != null)
-                {
-                    using var imageStream = await photo.OpenReadAsync();
-                    var result = await AzureOCRService.PerformOCRAsync(imageStream);
+        return null;
+    }
 
-                    if (result != null)
-                        return await fiscalCodeOCRAnalyzerService.GetFiscalCodeDTOFromOCRScanAsync(result.Lines);
-                }
-            }
+    public static async Task<ReadResult?> PerformOCRScanAsync(Stream stream)
+    {
+        try
+        {
+            return await AzureOCRService.PerformOCRAsync(stream);
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine(ex);
+        }
+
+        return null;
+    }
+
+    public async Task<FiscalCodeDTO> GetFiscalCodeDTOFromOCRScanAsync(ReadResult result)
+    {
+        try
+        {
+            return await fiscalCodeOCRAnalyzerService.GetFiscalCodeDTOFromOCRScanAsync(result.Lines);
         }
         catch (Exception ex)
         {
